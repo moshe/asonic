dd514bb07f4486460792e70fb2363f2406729c98
Moshe Zada <zada.moshe@gmail.com>
2019-03-25 13:12:07 +0200

    Add readme

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except Exception:
 setup(
     name='asonic',
     author='Moshe Zada',
-    version='0.1.0',
+    version='0.1.1',
     packages=['asonic'],
     keywords=['asonic', 'sonic', 'search', 'asyncio', 'async', 'text'],
     url='https://github.com/moshe/asonic',
