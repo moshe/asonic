f2d91f9e0af2ec5ed7a4b845dae5262b175ba521
Moshe Zada <zada.moshe@gmail.com>
2019-04-04 11:23:07 +0300

    bump version - 1.0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except Exception:
 setup(
     name='asonic',
     author='Moshe Zada',
-    version='1.0.1',
+    version='1.0.2',
     packages=['asonic'],
     keywords=['asonic', 'sonic', 'search', 'asyncio', 'async', 'text'],
     url='https://github.com/moshe/asonic',
